32b300f30c906badcae5ed5129baccc2e3b0d3b6
mikespub <mikespub@users.noreply.github.com>
2018-03-18 14:57:40 +0100

    add update script

---
+++ update_subtrees.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+git subtree pull -P publications xaraya com.xaraya.modules.bermuda.publications
+git subtree pull -P crispbb xaraya com.xaraya.modules.bermuda.crispbb
+git subtree pull -P scheduler xaraya com.xaraya.modules.bermuda.scheduler
+git subtree pull -P xarcachemanager xaraya com.xaraya.modules.jamaica.xarcachemanager
+git subtree pull -P workflow xaraya com.xaraya.modules.jamaica.workflow.2.1
+git subtree pull -P cacher xaraya com.xaraya.modules.cacher
+git status
